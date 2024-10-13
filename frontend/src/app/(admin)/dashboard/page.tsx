93df7fcc3f9e1bfd558de587137e23dc7ad81294
tkyj <82047680+tkyj@users.noreply.github.com>
2024-10-13 22:43:26 +0800

    added frontend

--- frontend/src/app/(admin)/dashboard/page.tsx
+++ frontend/src/app/(admin)/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { Form } from "@/app/components/Form";
 import PortUsage from "@/app/components/PortUsage";
 import { StatsGrid } from "@/app/components/StatsGrid";
 import { Flex, Space, Title } from "@mantine/core";
+import Image from 'next/image';
 import dynamic from "next/dynamic";
 
 export default function Dashboard() {
@@ -12,9 +13,12 @@ export default function Dashboard() {
   });
   return (
     <div className="p-6">
-      <Title order={3}>Dashboard</Title>
+    <div style={{ display: 'flex', alignItems: 'center', marginBottom: '1rem' }}>
+      <Image src="/icons/psa.png" alt="Logo" width={100} height={50} />
+      <Title order={3} style={{ marginLeft: '1rem' }}>Live AIS Dashboard</Title>
+    </div>
       {/* <Space h="lg" /> */}
-      <StatsGrid />
+      {/* <StatsGrid /> */}
       <Space h="lg" />
       <Map />
       {/* <Space h="lg" /> */}
